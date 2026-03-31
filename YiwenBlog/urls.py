76784beb0d0ee16574579e5cd3a15ec8b8fad208
YIWEN GE <ntxfgyw@163.com>
2026-03-31 23:30:19 +0800

    Front-end style optimization

--- YiwenBlog/urls.py
+++ YiwenBlog/urls.py
@@ -20,7 +20,7 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('myadmingyiwen/', admin.site.urls),
     path('', include('blog.urls', namespace='blog')),
 ]
 
