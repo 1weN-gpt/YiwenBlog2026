fbdd33cf2d92b2985f0f880e44c64836369c4530
YIWEN GE <ntxfgyw@163.com>
2026-04-01 23:58:54 +0800

    settings optimization

--- YiwenBlog/urls.py
+++ YiwenBlog/urls.py
@@ -15,17 +15,17 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.static import serve
 
 urlpatterns = [
     path('myadmingyiwen/', admin.site.urls),
     path('', include('blog.urls', namespace='blog')),
+    # 始终提供媒体文件服务，绕过 static() 的 DEBUG 限制
+    re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
 ]
-
-# 始终提供媒体文件服务（生产环境同样需要）
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 if settings.DEBUG:
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
